d77bf2e739f4af0d93a69d3103fe61b637d3b869
qd-qd <qdqdqdqdqd@protonmail.com>
2023-01-25 17:44:34 +0100

    🔧 setup eslint

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const port = process.env.PORT;
-console.log(port);
+const log = () => {
+    const port = process.env.PORT;
+    console.log(port);
+};
 
-export default () => { };
+export default log;
