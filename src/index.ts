650a9a2662786bb38b467380a89bbaabfde8cfc4
qd-qd <qdqdqdqdqd@protonmail.com>
2023-01-25 17:51:03 +0100

    🔧 setup prettier

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const log = () => {
-    const port = process.env.PORT;
-    console.log(port);
+  const port = process.env.PORT;
+  console.log(port);
 };
 
 export default log;
