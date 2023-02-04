7c1a8cffbb5114d7ba4c79ac0fb59720cbab69de
qd-qd <qdqdqdqdqd@protonmail.com>
2023-02-04 11:03:43 +0200

    ➖ clean dependencies
    - Remove telegraf as it is unused
    - Remove the package-lock.json in favour of pnpm-lock.yaml

--- src/plugins/telegram/telegramPlugin.ts
+++ src/plugins/telegram/telegramPlugin.ts
@@ -1,7 +1,5 @@
 import axios from 'axios';
 import * as dotenv from 'dotenv';
-import { NotificationPayload } from '../../utils/types';
-import Telegraf from 'telegraf';
 dotenv.config();
 
 const apiUrl = `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}`;
