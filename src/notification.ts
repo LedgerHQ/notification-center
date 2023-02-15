8d340b880195e5c8b84277b77b8c32ab3febe2bf
Alexandre Chabrolin <9203826+chabroA@users.noreply.github.com>
2023-02-15 08:37:39 +0100

    🎨 remove unecessary else clause
    else clause is unecessay here since there is an early return if the user
    is not found

--- src/notification.ts
+++ src/notification.ts
@@ -21,23 +21,22 @@ export async function sendNotifications(
   try {
     const user = await getUser(payload.to);
     if (!user) return null;
-    else {
-      const services = getServices(user);
-      if (services.telegrams?.length) {
-        // Sending notification message to the telegram handles
-        plugins.telegram.notify(payload.message, getHandles(user, 'telegrams'));
-      }
-      if (services.emails?.length) {
-        // Sending notification message to the email handles
-        // Uncomment this to work on mail notification plugin
-        // emailPlugin.notify(payload.message, getHandles(user, "emails"));
-      }
-      if (services.ifttts?.length) {
-        // Sending notification message to IFTTT
-        plugins.ifttt.notify(payload.message, getHandles(user, 'ifttts'));
-      }
-      return user as User;
+
+    const services = getServices(user);
+    if (services.telegrams?.length) {
+      // Sending notification message to the telegram handles
+      plugins.telegram.notify(payload.message, getHandles(user, 'telegrams'));
     }
+    if (services.emails?.length) {
+      // Sending notification message to the email handles
+      // Uncomment this to work on mail notification plugin
+      // emailPlugin.notify(payload.message, getHandles(user, "emails"));
+    }
+    if (services.ifttts?.length) {
+      // Sending notification message to IFTTT
+      plugins.ifttt.notify(payload.message, getHandles(user, 'ifttts'));
+    }
+    return user as User;
   } catch (err) {
     console.log('➡️ The following error ocurred: ', err);
     return null;
