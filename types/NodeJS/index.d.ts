ea6b4e6c143426e3804ee4b114dd590e30d23c64
Alexandre Chabrolin <9203826+chabroA@users.noreply.github.com>
2023-03-01 22:18:49 +0100

    🧑‍💻 make docker usage optionnal
    one can choose between running the app using docker compose or starting
    each component manually locally

--- types/NodeJS/index.d.ts
+++ types/NodeJS/index.d.ts
@@ -4,10 +4,23 @@ export {};
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      DB_URL: string;
+      // General / common env variables
+      USE_DOCKER?: string;
       TELEGRAM_TOKEN: string;
-      PORT?: string;
       MAX_TIME_DIFF?: string;
+
+      // Local specific env variables
+      LOCAL_DB_URL: string;
+      LOCAL_PORT?: string;
+
+      // Docker specific env variables
+      MONGODB_USER: string;
+      MONGODB_PASSWORD: string;
+      MONGODB_DATABASE: string;
+      MONGODB_LOCAL_PORT: string;
+      MONGODB_DOCKER_PORT: string;
+      NODE_LOCAL_PORT: string;
+      NODE_DOCKER_PORT: string;
     }
   }
 }
